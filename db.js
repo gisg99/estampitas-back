bca5294788c8994a013ddde50b2dca6f643e432d
Gabriel Sanchez <gisgarci99@gmail.com>
2026-05-10 18:26:43 -0600

    A!

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 import pg from 'pg';
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DATABASE_URL; 
 
 const isLocal = !connectionString || /localhost|127\.0\.0\.1/.test(connectionString);
 
